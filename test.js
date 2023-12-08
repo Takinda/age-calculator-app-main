d21931ef9dd181dbdea430e86bd373213e2e4abe
Ghanim Ali <67353648+Takinda@users.noreply.github.com>
2023-12-08 22:46:10 +0300

    Update test.js
    fixing day

--- test.js
+++ test.js
@@ -52,7 +52,7 @@ function Getbirth(){
             }
     if ((M==1 || M==3||M==5||M==7||M==8||M==10||M==12 )&&Y<=TY) { 
         console.log("Nice");
-           if(D>0 && D<31){
+           if(D>0 && D<=31){
             console.log("Nice");
             
             document.getElementById("year").textContent = `${nowyearage}`;
